7dbde580d4663d16ac40e628c23c44ddc1e9be0e
Andrew Stevenson <129066987+AndrewStevenson550@users.noreply.github.com>
2025-11-17 13:56:27 +0000

    normal stuff

---
+++ simple-counter-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='title'>
+        <h1>Simple counter App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
